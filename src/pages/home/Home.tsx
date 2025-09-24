be6bdc7c15ae9662557f0272c85aa2bacdb0613e
dreammehedi <dreammehedihassan@gmail.com>
2025-09-24 23:06:27 +0600

    Basic Project Setup

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import { Helmet } from "@dr.pogodin/react-helmet";
+
+function Home() {
+  return (
+    <>
+      <Helmet>
+        <title>Home | ScapeSync</title>
+      </Helmet>
+      <h1 className="container mx-auto"> Home</h1>
+    </>
+  );
+}
+
+export default Home;
